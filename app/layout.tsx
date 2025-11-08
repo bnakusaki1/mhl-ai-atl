b7f8b51207566d18395a017b468a5e2e3acfc2a8
bnakusaki <akusakibenjaminnartey@gmail.com>
2025-11-07 22:36:24 -0500

    Authentication with Google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Header } from "./login/page";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +14,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <style>
+          {
+            "@import url('https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap');"
+          }
+        </style>
+      </head>
+      <body className={`urbanist antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
